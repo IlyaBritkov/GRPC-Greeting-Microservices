a42d2c0dc7c3b4e9b80b6176f6190bc167d362c2
Ilya Britkov <ilya.britkov@sap.com>
2022-04-29 21:22:38 +0300

    small clean up in Server App file

--- GRPC_Java_Server/src/main/java/com/server/App.java
+++ GRPC_Java_Server/src/main/java/com/server/App.java
@@ -6,16 +6,13 @@ import io.grpc.ServerBuilder;
 import java.io.IOException;
 
 public class App {
-
     public static void main(String[] args) throws IOException, InterruptedException {
         final Server server = ServerBuilder.forPort(8080)
                 .addService(new GreetingServiceImpl())
                 .build();
 
         server.start();
-
         System.out.println("Server has started");
-
         server.awaitTermination();
     }
 }
